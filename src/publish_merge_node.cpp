01d565eb8c41f198ba854a7f49342c7a71d2ef13
Your Name <you@example.com>
2020-01-06 15:22:21 +0800

    01060

--- src/publish_merge_node.cpp
+++ src/publish_merge_node.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
   
   ros::Publisher pub = n.advertise<dslam_sp::image_depth>("/merge/img_depth_file", 1); //创建publisher，往话题上发布消息
   ros::Publisher pub_info = n.advertise<sensor_msgs::CameraInfo>("/mynteye/left_rect/camera_info", 1); //创建publisher，往话题上发布消息
-  ros::Rate loop_rate(5);   //定义发布的频率，20HZ 
+  ros::Rate loop_rate(20);   //定义发布的频率，20HZ 
   
   while (ros::ok())   //循环发布msg
   {
@@ -53,9 +53,11 @@ int main(int argc, char **argv)
     img_depth_msg.depth = *ptr;
     
     cout << "publish: " << tmp << endl;
-    pub.publish(img_depth_msg);//以1Hz的频率发布msg
-    pub_info.publish(camera_info);//以1Hz的频率发布msg
-    loop_rate.sleep();//根据前面的定义的loop_rate,设置1s的暂停
+    if ( tmp %3 == 0){
+      pub.publish(img_depth_msg);//以1Hz的频率发布msg
+      pub_info.publish(camera_info);//以1Hz的频率发布msg
+      loop_rate.sleep();//根据前面的定义的loop_rate,设置1s的暂停
+    }
     
     // cv_bridge::CvImageConstPtr cv_ptr1;
     // sensor_msgs::ImagePtr img_ptr = boost::make_shared<::sensor_msgs::Image>(img_depth_msg.image);
